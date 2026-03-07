a0a6172c92b7854d3c045c0a2185f2506f9df4f5
mf_andreich <andreich-ne@yandex,ru>
2026-03-08 01:09:02 +0700

    Move TLDs filters to metacube

--- providers/metaCubeGeosite.js
+++ providers/metaCubeGeosite.js
@@ -5,6 +5,9 @@ const logTag = "metacube";
 
 const downloadUrl = "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/sing/geo/geosite/{{service}}.json"
 
+const excludedTlds = "cv|dj|dm|im|kg|ki|li|ml|ms|mv|ne|nr|sm|ad|as|bf|bj|bt|cd|cf|ci|ao|bw|ck|ls|mz|vi|zm|bn|bz|cy|et|fj|gi|kh|mm|na|np|pg|sb|sl|vc|mg|ac|af|ag|ai|bi|bs|cg|cm|cu|dz|ga|gd|gl|gm|gs|gy|ht|je|lc|mp|mu|mw|nu|pn|re|rw|sc|sr|st|sx|sy|tf|tj|tl|tt|vg|vu|wf|yt|do|ec|eg|gh|hn|jm|kw|lb|mt|om|py|tr|ae|al|am|at|bg|ch|id|ve|uk|za|zw|ar|au|bd|br|il|ke|nz|th|tz|de|es|fr|gr|hr|hu|ie|is|it|ng|pl|ro|rs|sa|ua|jo|uz|tm|az|ba|bh|bo|by|ca|qa|vn|uy|ug|tn|sv|sk|si|sg|ee|sn|cl|pt|pr|pk|ph|pe|pa|no|ni|my|mx|mn|mk|md|ma|ly|lv|lu|lt|lk|la|kz|kr|iq|in|hk|gt|ge|fi|cr|cz|dk";
+const excludedRegex = new RegExp(`\\.(${excludedTlds})$`, 'i');
+
 async function init() {
 
 }
@@ -43,7 +46,7 @@ async function getDomainsForService(service, resultList = []){
             processDomainField(rule.domain_suffix , domains);
         }
 
-        domains = filterDomains(domains);
+        domains = filterDomains(domains).filter(domain => !excludedRegex.test(domain));
 
         let pushed = 0;
 
--- utils.js
+++ utils.js
@@ -1,7 +1,4 @@
-﻿const excludedTlds = "cv|dj|dm|im|kg|ki|li|ml|ms|mv|ne|nr|sm|ad|as|bf|bj|bt|cd|cf|ci|ao|bw|ck|ls|mz|vi|zm|bn|bz|cy|et|fj|gi|kh|mm|na|np|pg|sb|sl|vc|mg|ac|af|ag|ai|bi|bs|cg|cm|cu|dz|ga|gd|gl|gm|gs|gy|ht|je|lc|mp|mu|mw|nu|pn|re|rw|sc|sr|st|sx|sy|tf|tj|tl|tt|vg|vu|wf|yt|do|ec|eg|gh|hn|jm|kw|lb|mt|om|py|tr|ae|al|am|at|bg|ch|id|ve|uk|za|zw|ar|au|bd|br|il|ke|nz|th|tz|de|es|fr|gr|hr|hu|ie|is|it|ng|pl|ro|rs|sa|ua|jo|uz|tm|az|ba|bh|bo|by|ca|qa|vn|uy|ug|tn|sv|sk|si|sg|ee|sn|cl|pt|pr|pk|ph|pe|pa|no|ni|my|mx|mn|mk|md|ma|ly|lv|lu|lt|lk|la|kz|kr|iq|in|hk|gt|ge|fi|cr|cz|dk";
-const excludedRegex = new RegExp(`\\.(${excludedTlds})$`, 'i');
-
-export function isValidDomain(domain) {
+﻿export function isValidDomain(domain) {
     if (typeof domain !== "string") {
         console.error(`Not valid domain: ${domain}`);
         return false;
@@ -34,5 +31,5 @@ export function isValidDomain(domain) {
 }
 
 export function filterDomains(domains){
-    return domains.filter(domain => !excludedRegex.test(domain)).filter(domain => domain.split('.').length > 1).filter(domain => isValidDomain(domain));
+    return domains.filter(domain => domain.split('.').length > 1).filter(domain => isValidDomain(domain));
 }
